e3105f24c338aeb17b552f14cebf92738ff929a9
Luis <luisced02031@gmail.com>
2022-04-20 20:45:27 -0500

    Agregando clase de usuario

--- weekly_mission_3/MyFirstProject/Test/user.test.js
+++ weekly_mission_3/MyFirstProject/Test/user.test.js
@@ -1,5 +1,11 @@
+const User = require("./app/models/User");
+
 describe("prueba de jest", () => {
-    test("Prueba 1", () => {
-        expect(10).toBe(9)
+    test("Creación de user", () => {
+        const user = new User(1, "luiscedillo", "Bio", "2022-02-02", "2022-02-02");
+
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("luiscedillo")
+
     })
 })
